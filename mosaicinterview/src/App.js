1d6123bbc31d2aca9fa44e55e0dff44192a53a35
lena <lena@floored.com>
2020-06-03 10:20:19 -0400

    add search input so filter

--- mosaicinterview/src/App.js
+++ mosaicinterview/src/App.js
@@ -6,32 +6,49 @@ import { connect } from 'react-redux';
 
 
 const API_KEY = '7ffff7bc93f34291a600f8d927d1ad96'; // move this into env.json
-const baseUrl = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
+const baseUrl = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`;
+const searchBaseUrl = `https://newsapi.org/v2/everything?apiKey=${API_KEY}`;
 
 function App({ fetchNews, hasError, isLoading, data }) {
-  const [url, setUrl] = useState(baseUrl);
+  const [searchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
-    fetchNews(url);
-  }, [url, fetchNews]) // hmmm, having fetchNews in Here
+    fetchNews(baseUrl);
+  }, [fetchNews]) // hmmm, having fetchNews in Here
 
-  console.log(data)
+  function handleSubmit(evt) {
+    evt.preventDefault();
+    fetchNews(`${searchBaseUrl}&q=${searchTerm}`);
+  }
 
   return (
     <main className="App">
       <header className="App-header">
-       {hasError ? <div>Some error happened</div> :
-        isLoading ? <div>Loading.</div> :
-         <ul>
-           {
-             data.articles.map(a =>
-               <li key={a.publishedAt}>
-                 <a href={a.url} target="_blank">{a.title}</a>
-               </li>
-              )
-           }
-         </ul>
-       }
+        <div>
+
+         {hasError ? <div>Some error happened</div> :
+          isLoading ? <div>Loading.</div> :
+           <ul>
+             { data &&
+               data.articles.map(a =>
+                 <li key={a.publishedAt}>
+                   <a href={a.url} target="_blank" rel="noopener noreferrer">{a.title}</a>
+                 </li>
+                )
+             }
+           </ul>
+         }
+        </div>
+       <form onSubmit={handleSubmit}>
+         <label htmlFor="searchInput">Find news about:</label>
+         <input
+           type="text"
+           value={searchTerm}
+           id="searchInput"
+           onChange={e => setSearchTerm(e.target.value)}
+         />
+         <button type="submit">Submit</button>
+       </form>
       </header>
     </main>
   );
